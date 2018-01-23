9296e8bf767f825f615f79101775309a724c9a18
Andrew Max <anm898989@gmail.com>
2018-01-23 14:30:55 +0200

    new and create quotes actions

--- web/controllers/quote_controller.ex
+++ web/controllers/quote_controller.ex
@@ -1,15 +1,32 @@
 defmodule Splurty.QuoteController do
   use Phoenix.Controller
 
+  alias Splurty.Router
+  import Splurty.Router.Helpers
+
   plug :action
 
   def homepage(conn, _params) do
-    render conn, "homepage.html"
+    conn
+    |>assign(:x, "xxxxxxxx")
+    |>assign(:y, "yyyyyyy")
+    |>render("homepage.html")
   end
 
   def index(conn, _params) do
     conn
     |> assign(:quotes, Repo.all(Splurty.Quote))
     |> render("index.html")
+  end
+
+  def new(conn, _params) do
+    render conn, "new.html"
+  end
+
+  def create(conn, %{"quote" => %{"saying" => saying, "author" => author}}) do
+    q = %Splurty.Quote{saying: saying,  author: author}
+    Repo.insert(q)
+
+    redirect conn, to: quote_path(conn, :index)
   end
 end
